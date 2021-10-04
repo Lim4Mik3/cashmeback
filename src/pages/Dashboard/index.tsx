34982cf189a06b564d912d6c6c31c70a838085b5
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-04 16:29:45 -0300

    feat: Implemeting dashboard page and summary cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
+import { Flex, Box, Text, SimpleGrid, Icon, Img } from '@chakra-ui/react'
+import { GiReceiveMoney } from 'react-icons/gi'
+import { FaPercent } from 'react-icons/fa'
+
+import incomeImg from '../../assets/income.svg'
 
 export function Dashboard() {
   return (
@@ -40,13 +44,130 @@ export function Dashboard() {
         as="main"
         maxW={1280}
         w="100%"
-        // bg="whitesmoke"
+        bg="white"
         h="100%"
         my="0"
         mx="auto"
-        p="4"
+        py="6"
+        px="4"
       >
-        
+        <SimpleGrid
+          w="100%"
+          h="36"
+          minChildWidth={360}
+          flexDirection="row"
+          gap="12"
+        >
+          <Flex
+            w="100%"
+            h="36"
+            bg="facebook.50"
+            px="10"
+            py="2"
+            borderRadius={12}
+            boxShadow="lg"
+            direction="column"
+            justify="space-between"
+          >
+            <Box
+              as="header"
+              display="flex"
+              alignContent="center"
+              justifyContent="space-between"
+              pt="4"
+            >
+              <Text
+                textAlign="left"
+                color="gray.600"
+                fontWeight="thin"
+                fontSize="26"
+              >Vendas</Text>
+              <Img src={incomeImg} />
+            </Box>
+
+            <Text
+              mt="auto"
+              textAlign="left"
+              color="gray.600"
+              fontWeight="500"
+              fontSize="34"
+            >280 vendas</Text>
+          </Flex>          
+          <Flex
+            w="100%"
+            h="36"
+            bg="facebook.50"
+            px="10"
+            py="2"
+            borderRadius={12}
+            boxShadow="lg"
+            direction="column"
+            justify="space-between"
+          >
+            <Box
+              as="header"
+              display="flex"
+              alignContent="center"
+              justifyContent="space-between"
+              pt="4"
+            >
+              <Text
+                textAlign="left"
+                color="gray.600"
+                fontWeight="thin"
+                fontSize="26"
+              >Lucro estimado</Text>
+              <Icon as={GiReceiveMoney} color="green.500" fontSize="30" />
+            </Box>
+
+            <Text
+              mt="auto"
+              textAlign="left"
+              color="gray.600"
+              fontWeight="500"
+              fontSize="34"
+            >R$ 15.280,00</Text>
+          </Flex>          
+          <Flex
+            w="100%"
+            h="36"
+            bg="facebook.50"
+            px="10"
+            py="2"
+            borderRadius={12}
+            boxShadow="lg"
+            direction="column"
+            justify="space-between"
+          >
+            <Box
+              as="header"
+              display="flex"
+              alignContent="center"
+              justifyContent="space-between"
+               pt="4"
+            >
+              <Text
+                textAlign="left"
+                color="gray.600"
+                fontWeight="thin"
+                fontSize="26"
+              >Cashback adquirido</Text>
+              <Icon as={FaPercent} color="green.500" fontSize="30" />
+            </Box>
+
+            <Text
+              mt="auto"
+              textAlign="left"
+              color="gray.600"
+              fontWeight="500"
+              fontSize="34"
+            >180%</Text>
+          </Flex>          
+          
+              
+                  
+                   
+        </SimpleGrid>        
       </Flex>
 
 
