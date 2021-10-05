ec00bed6bccb4fef94f73c8c510e598454ee1ef8
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-05 09:38:37 -0300

    fix: Refactoring Login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,94 +1,50 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Box, Flex, Text, VStack, Button, Divider } from "@chakra-ui/react"
+import { Flex, Text, VStack, Button } from "@chakra-ui/react"
 import { Input } from "../../components/Form/Input"
+import { Logo } from '../../components/Logo';
 
 export function Login() {
   const userHistory = useHistory();
 
   return (
-    <Box
-      h="100vh"
-      w="100vw"
-      display="flex"
-      alignContent="center"
-      justifyContent="center"
-      p="6"
-    >
-      <Flex
-        w={520}
-        align="center"
-        justify="center"
-      >
+    <Flex h="100vh" w="100vw" justifyContent="center">
+      <Flex w={520} align="center" justify="center">
         <Flex
           direction="column"
           bg="gray.200"
-          w="80%"
+          maxW={520}
+          w="100%"
           p="10"
           borderRadius={8}
           align="center"
           justify="center"
           boxShadow="2xl"
         >
+          <Logo />          
+
           <Text
-            fontSize="30"
-            color="gray.900"
-            fontWeight="light"
-            letterSpacing="tight"
-          >Cashme
-            <Text
-              fontSize="30"
-              color="whatsapp.500"
-              display="inline-block"
-              fontWeight="bold"
-            >
-              BACK 🤑
+            fontSize="18"
+            py="2"
+            color="blue.600"
+            letterSpacing="tighter"
+            textTransform="uppercase"
+          >
+            Faça login revendedor
           </Text>
-        </Text>
 
-        <Text
-          fontSize="18"
-          py="2"
-          color="blue.600"
-          letterSpacing="tighter"
-          textTransform="uppercase"
-        >Faça login revendedor</Text>
+          <VStack my="6" w="100%">
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Senha" />
+          </VStack>
 
-        <VStack
-          my="6"
-          w="100%"
-        >
-          <Input 
-            name="email" 
-            type="email" 
-            label="E-mail"
-          />
-          <Input 
-            name="password" 
-            type="password" 
-            label="Senha"
-          />
-        </VStack>
+          <Button colorScheme="whatsapp" w="100%" mb="4">Entrar</Button>
 
-        <Button
-          colorScheme="whatsapp"
-          w="100%"          
-        >Entrar</Button>
-
-        <Divider colorScheme="pink" py="2" size="lg" />
-
-        <Button
-          w="100%"   
-          colorScheme="linkedin"
-          onClick={() => userHistory.push("/signup")}
-        >
-          Criar uma conta
-        </Button>
-
-        
+          <Button w="100%" colorScheme="linkedin"onClick={() => userHistory.push("/signup")}>
+            Criar uma conta
+          </Button>
         </Flex>
-
       </Flex>
-    </Box>
+    </Flex>
   )
 }
