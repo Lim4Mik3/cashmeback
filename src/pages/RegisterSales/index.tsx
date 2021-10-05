7bfae2f1f3e3aaed1be64bc513f88a525989985f
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-05 11:01:01 -0300

    feat: Created register sale page and thei navigation

--- src/pages/RegisterSales/index.tsx
+++ src/pages/RegisterSales/index.tsx
@@ -1,12 +1,65 @@
 import React from 'react'
-import { Text } from "@chakra-ui/react"
+import { Text, Flex, Button, Icon, VStack } from "@chakra-ui/react"
+import { MdArrowBack } from 'react-icons/md'
 
 import { PrivateLayout } from '../../components/Layouts/PrivateLayout'
+import { Input } from '../../components/Form/Input'
+import { useHistory } from 'react-router'
 
 export function RegisterSales() {
+  const userHistory = useHistory();
+
   return (
     <PrivateLayout>
-      <Text color="gray.900">Bem vindo ao register sales</Text>
+      <Flex w="100%" align="center" justifyContent="space-between">
+        <Button 
+          colorScheme="blue" 
+          leftIcon={<Icon as={MdArrowBack} fontSize="20" />} 
+          color="gray.900"
+          onClick={() => userHistory.push('/dashboard')}
+        >
+          Voltar
+        </Button>
+
+        <Text 
+          fontSize="22" 
+          color="whatsapp.800" 
+          textTransform="uppercase" 
+          fontWeight="700"
+        >
+          Cadastrar nova venda
+        </Text>
+
+        <Text fontSize="16" textAlign="right" color="gray.500">
+          250 vendas <br /> cadastradas.
+        </Text>
+      </Flex>
+
+      <Flex
+        align="center"
+        direction="column"
+        h="100%"
+        mt="20"
+        >
+        <VStack
+          pt="10"
+          w="96"
+          spacing="6"
+        >  
+          <Input name="salecode" type="number" label="Código da compra"  />
+          <Input name="value" type="number" label="Valor da compra" />
+          <Input name="data" type="date" label="Data da venda" />
+          <Button
+            colorScheme="whatsapp"
+            w="100%"
+            size="lg"
+          > 
+            Cadastrar venda
+          </Button>
+        </VStack>
+
+      </Flex>
+
     </PrivateLayout>
   )
 }
