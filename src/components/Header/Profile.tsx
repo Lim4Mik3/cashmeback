c310a4d80a4c1a61c00ad6e8c1696af5d048636a
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-05 11:11:48 -0300

    fix: Refactoring header, include logoff button in the profile picture place

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import { Box, Text, Avatar } from '@chakra-ui/react'
+import { Box, Text, Icon, Button } from '@chakra-ui/react'
+import { CgLogOff } from 'react-icons/cg'
  
 export function Profile() {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-    >
+    <Box display="flex" alignItems="center">
       <Text
         fontSize="18"
         color="gray.900"
         textAlign="right"
         lineHeight="6"
       >
-        Leonardo Oliveira
-        <Text
-          display="block"
-          fontSize="12"
+      Leonardo Oliveira
+        <Text 
+          color="whatsapp.700" 
+          display="block" 
+          fontSize="12" 
+          fontWeight="bold" 
+          letterSpacing="wider"
         >
-          <Text color="whatsapp.700" fontSize="12" fontWeight="bold" letterSpacing="wider">REVENDEDOR</Text>
+          REVENDEDOR
         </Text>
       </Text>
-      <Avatar ml="4" name="Leonardo Oliveira"/>
+      <Button
+        w="50px"        
+        h="50px"        
+        ml="4"
+        borderRadius="full"
+        bg="whatsapp.700"
+        align="center"
+        justify="center"
+        title="Sair"
+        _hover={{
+          bg: "whatsapp.800"
+        }}
+      >
+        <Icon as={CgLogOff} fontSize="26" fontWeight="bold" />
+      </Button>
     </Box>
   )
 }
--- src/components/SalesTable/ActionsTable.tsx
+++ src/components/SalesTable/ActionsTable.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Flex, Text, Button, Icon } from '@chakra-ui/react'
 import { GrAddCircle } from 'react-icons/gr'
+import { useHistory } from 'react-router'
 
 export function ActionsTable() {
+  const userHistory = useHistory();
+
   return (
     <Flex align="center" justify="space-between" w="100%">
       <Text fontSize="18" color="gray.900" fontWeight="700" textTransform="uppercase">
@@ -14,6 +17,7 @@ export function ActionsTable() {
         fontSize="15"
         alignContent="center"
         rightIcon={<Icon as={GrAddCircle} fontSize="18" color="red" />}
+        onClick={() => userHistory.push('/sale')}
       >
         Cadastrar nova venda
       </Button>
