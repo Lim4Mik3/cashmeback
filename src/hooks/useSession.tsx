e2cb7db5510787368c33dbe7fcb0031caa25b5aa
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-06 16:11:36 -0300

    feat: Implementing user session hook and their functions(Login)

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -1,21 +1,49 @@
-import React, { createContext, useContext } from 'react'
-import { ReactNode } from 'react-router/node_modules/@types/react';
+import React, { createContext, useContext, useState, ReactNode } from 'react'
+import { useHistory } from 'react-router-dom'
+import { api } from '../services/api';
 
 interface SessionProviderProps {
   children: ReactNode;
 }
 
 interface SessionContextData {
-  userLogged: boolean;
+  Login: (data: userLoginInput) => Promise<void>;
 }
+
+interface userData {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  CPF: string;
+}
+
+type userLoginInput = Pick<userData, 'email' | 'password'>
 
 const SessionContext = createContext<SessionContextData>({} as SessionContextData);
 
 export function SessionProvider({ children }: SessionProviderProps) {
-  const userLogged = false
+  const [user, setUser] = useState<userData>({} as userData)
+  const browserHistory = useHistory();
+
+  async function Login({ email, password }: userLoginInput) {
+    const response = await api.get<userData>(`/users?email=${email}`)
+
+    if (!!response.data) {
+      console.log("nao há nenhum usuario com esse email");
+      return;
+    }
+
+    const user: userData = response.data;
+
+    if (user.email === email && user.password === password) {
+      setUser(user);
+      browserHistory.push('/dashboard');
+    }
+  }
 
   return (
-    <SessionContext.Provider value={{ userLogged }}>
+    <SessionContext.Provider value={{ Login }}>
       {children}
     </SessionContext.Provider>
   )
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Flex, Text, VStack, Button } from "@chakra-ui/react"
 import { Input } from "../../components/Form/Input"
 import { Logo } from '../../components/Logo';
 import { PublicLayout } from '../../components/Layouts/PublicLayout';
+import { useSession } from '../../hooks/useSession';
+import { FormEvent } from 'react-router/node_modules/@types/react';
 
 export function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const userHistory = useHistory();
+  const { Login } = useSession();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if(!(email.trim())) {
+      return;
+    }
+
+    await Login({ email, password })
+  }
+
 
   return (
     <PublicLayout>
@@ -32,16 +48,16 @@ export function Login() {
           Faça login revendedor
         </Text>
 
-        <VStack my="6" spacing={["2", "4"]} w="100%">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
-        </VStack>
+          <VStack my="6" spacing={["2", "4"]} w="100%">
+            <Input name="email" type="email" label="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Input name="password" type="password" label="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </VStack>
 
-        <Button colorScheme="whatsapp" w="100%" mb="4">Entrar</Button>
+          <Button colorScheme="whatsapp" w="100%" mb="4" onClick={handleSubmit}>Entrar</Button>
 
-        <Button w="100%" colorScheme="linkedin" onClick={() => userHistory.push("/signup")}>
-          Criar uma conta
-        </Button>
+          <Button w="100%" colorScheme="linkedin" onClick={() => userHistory.push("/signup")}>
+            Criar uma conta
+          </Button>
       </Flex>
     </PublicLayout>
   )
